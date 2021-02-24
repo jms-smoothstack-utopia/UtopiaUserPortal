7cce5955d874c874162ee2eb993b29284577b9e5
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-02-24 14:40:25 -0500

    initial basic UserService tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'UtopiaUserPortal'`, () => {
+  it(`should have as title 'Utopia User Portal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('UtopiaUserPortal');
+    expect(app.title).toEqual('Utopia User Portal');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('UtopiaUserPortal app is running!');
-  });
+  //the base-level app component contains no useful content
+  //so not testing for that
 });
--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
@@ -6,11 +7,22 @@ describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [],
+      providers: [UserService]
+    });
     service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('getUser 2 should return user 2', (done: DoneFn) => {
+    service.getUser(2).subscribe(returned => {
+      expect(returned).toBeTruthy();
+      done();
+    });
+  })
 });
