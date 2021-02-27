2cb1633ffdd14cf1c5c1641731a7774b9316bc86
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-02-27 12:17:41 -0500

    Add unit tests to account creation
    Navigate to '/' if logged in
    Fix broken user profile/service tests

--- src/app/account/create-account/create-account.component.spec.ts
+++ src/app/account/create-account/create-account.component.spec.ts
@@ -19,7 +19,12 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 import PathConstants from '../../../environments/paths';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
+//TODO: Test error case for duplicate email.
+// I cannot for the life of me figure out how to mock an error response from the
+// observable from the account service.
 describe('CreateAccountComponent', () => {
   let component: CreateAccountComponent;
   let fixture: ComponentFixture<CreateAccountComponent>;
@@ -66,6 +71,41 @@ describe('CreateAccountComponent', () => {
   afterEach(() => {
     component.accountForm.reset();
   });
+
+  const setFormValidAndDetectChanges = () => {
+    component.accountForm.get('email')!.setValue('test@test.com');
+    expect(component.accountForm.get('email')!.valid).toBeTrue();
+
+    component.accountForm.get('firstName')!.setValue('John');
+    expect(component.accountForm.get('firstName')!.valid).toBeTrue();
+
+    component.accountForm.get('lastName')!.setValue('Sample');
+    expect(component.accountForm.get('lastName')!.valid).toBeTrue();
+
+    component.accountForm.get('addrLine1')!.setValue('123 Main St.');
+    expect(component.accountForm.get('addrLine1')!.valid).toBeTrue();
+
+    component.accountForm.get('phoneNumber')!.setValue('4178926530');
+    component.onPhoneNumberFocusOut();
+    expect(component.accountForm.get('phoneNumber')!.valid).toBeTrue();
+
+    component.accountForm.get('city')!.setValue('Las Vegas');
+    expect(component.accountForm.get('city')!.valid).toBeTrue();
+
+    component.accountForm.get('state')!.setValue('Nevada');
+    expect(component.accountForm.get('state')!.valid).toBeTrue();
+
+    component.accountForm.get('zipcode')!.setValue('98765');
+    expect(component.accountForm.get('zipcode')!.valid).toBeTrue();
+
+    component.accountForm.get('password')!.setValue('abCD1234!@');
+    expect(component.accountForm.get('password')!.valid).toBeTrue();
+
+    component.accountForm.get('confirmPassword')!.setValue('abCD1234!@');
+    expect(component.accountForm.get('confirmPassword')!.valid).toBeTrue();
+
+    fixture.detectChanges();
+  };
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -180,29 +220,93 @@ describe('CreateAccountComponent', () => {
     });
   });
 
-  it('TODO: #onSubmit happy path', () => {
-    //TODO
-    fail();
+  it('TODO: #onSubmit happy path', async () => {
+    setFormValidAndDetectChanges();
+    const mockResponse = {
+      id: '1234',
+      firstName: 'John',
+      lastName: 'Sample',
+      phoneNumber: '417-892-6530',
+      email: 'test@test.com',
+      addresses: [
+        {
+          cardinality: 1,
+          line1: '123 Main St.',
+          line2: undefined,
+          city: 'Las Vegas',
+          state: 'Nevada',
+          zipcode: '98765',
+        },
+      ],
+      loyaltyPoints: 0,
+    };
+
+    spyOn(accountServiceSpy, 'createAccount').and.returnValue(of(mockResponse));
+    const login = spyOn(authServiceSpy, 'login');
+    const navigation = spyOn(mockRouter, 'navigate');
+
+    component.onSubmit();
+
+    expect(login).toHaveBeenCalledWith('test@test.com', 'abCD1234!@');
+    expect(navigation).toHaveBeenCalledWith([
+      PathConstants.USER_PROFILE,
+      mockResponse.id,
+    ]);
+
+    expect(component.isLoading).toBeFalse();
+    expect(component.errorMsg).toBeUndefined();
   });
 
-  it('TODO submit button should be disabled when form is invalid', () => {
-    //TODO
-    fail();
+  it('submit button should be disabled at initialization', () => {
+    const button = fixture.debugElement.query(By.css('#submitButton'));
+    expect(button).toBeTruthy();
+    expect(button.properties.disabled).toBeTrue();
   });
 
-  it('TODO: #selectedStateAbbreviation', () => {
-    //TODO
-    fail();
+  it('submit button should be enabled when form is valid', () => {
+    setFormValidAndDetectChanges();
+
+    const button = fixture.debugElement.query(By.css('#submitButton'));
+    expect(button).toBeTruthy();
+    expect(button.properties.disabled).toBeFalse();
   });
 
-  it('TODO: #getErrorText', () => {
-    //TODO
-    fail();
+  it('#selectedStateAbbreviation returns an abbreviation from the selected state', () => {
+    component.accountForm.get('state')!.setValue('Georgia');
+    let result = component.selectedStateAbbreviation;
+    expect(result).toEqual('GA');
+
+    component.accountForm.get('state')!.setValue('Nevada');
+    result = component.selectedStateAbbreviation;
+    expect(result).toEqual('NV');
+
+    component.accountForm.get('state')!.setValue('District Of Columbia');
+    result = component.selectedStateAbbreviation;
+    expect(result).toEqual('DC');
   });
 
-  it('TODO: #onCloseAlert', () => {
-    //TODO
-    fail();
+  it('#selectedStateAbbreviation should return empty string if no selection', () => {
+    let result = component.accountForm.get('state')!.value;
+    expect(result).toBe('');
+  });
+
+  it('#getErrorText should return a non-blank string for every required field', () => {
+    expectedFormFields.forEach((field) => {
+      if (field !== 'addrLine2') {
+        const errorText = component.getErrorText(field);
+        expect(errorText.trim()).toBeTruthy(field + ' failed to return text.');
+      }
+    });
+  });
+
+  it('#onCloseAlert should clear the component error message', () => {
+    component.errorMsg = undefined;
+    component.onCloseAlert();
+    expect(component.errorMsg).toBeUndefined();
+
+    component.errorMsg = 'some error';
+    component.onCloseAlert();
+    expect(component.errorMsg).toBeUndefined();
   });
 
   it('should redirect to profile if already logged in', () => {
