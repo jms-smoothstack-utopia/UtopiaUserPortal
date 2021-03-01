2ed0c7c57573b47ecb98bed6f3b2ede5ad146ee4
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-03-01 15:22:25 -0500

    Fix issue with resetting form during tests
    Remove resetting form on submit, no need to do so.

--- src/app/login/password-reset/password-reset.component.ts
+++ src/app/login/password-reset/password-reset.component.ts
@@ -3,41 +3,39 @@ import { NgForm } from '@angular/forms';
 import { NGXLogger } from 'ngx-logger';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
-import { HttpService } from "../../shared/methods/http.service";
+import { HttpService } from '../../shared/methods/http.service';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-password-reset',
   templateUrl: './password-reset.component.html',
-  styleUrls: ['./password-reset.component.css']
+  styleUrls: ['./password-reset.component.css'],
 })
 export class PasswordResetComponent implements OnInit {
   errorMsg?: string = undefined;
   moreInfoMsg?: string = undefined;
   isLoading = false;
-  form: NgForm | undefined;
+
   constructor(
-    private log:NGXLogger,
+    private log: NGXLogger,
     private httpService: HttpService,
     private router: Router
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onSubmitPasswordReset(passwordResetForm: NgForm){
-    this.form = passwordResetForm;
-    if (!passwordResetForm.valid){
-      this.log.debug("Invalid form");
-      this.errorMsg = "Please enter a valid email to reset your password";
-      return
+  onSubmitPasswordReset(passwordResetForm: NgForm) {
+    if (!passwordResetForm.valid) {
+      this.log.debug('Invalid form');
+      this.errorMsg = 'Please enter a valid email to reset your password';
+      return;
     }
 
     const email = passwordResetForm.value.email;
-    
+
     this.isLoading = true;
 
-    let url = environment.accountsEndpoint + "/password-reset";
+    let url = environment.accountsEndpoint + '/password-reset';
 
     this.httpService.post(url, email).subscribe(
       () => {
@@ -50,33 +48,27 @@ export class PasswordResetComponent implements OnInit {
     );
   }
 
-  resetPageForm(){
-    if(this.form != undefined){
-      this.form.reset();
-    }
-  }
-
-  async successPasswordReset(){
+  async successPasswordReset() {
     this.errorMsg = undefined;
     this.isLoading = false;
     await this.router.navigateByUrl('login/forgotpassword/checkemail');
   }
 
-  errorPasswordReset(err: HttpErrorResponse){
-    this.log.debug("Failure", err);
+  errorPasswordReset(err: HttpErrorResponse) {
+    this.log.debug('Failure', err);
     this.isLoading = false;
-    if (err.status == 404){
-      this.errorMsg = "The email was not found. Please confirm your email!"
-      this.moreInfoMsg = "<a href='login/accountmaker'>Click here to sign up!</a><hr><a href='login'>Click here to login!</a>";
-    } else{
-      this.errorMsg = "An error occurred while processing your request. Please try again."
+    if (err.status == 404) {
+      this.errorMsg = 'The email was not found. Please confirm your email!';
+      this.moreInfoMsg =
+        "<a href='login/accountmaker'>Click here to sign up!</a><hr><a href='login'>Click here to login!</a>";
+    } else {
+      this.errorMsg =
+        'An error occurred while processing your request. Please try again.';
     }
-    this.resetPageForm();
   }
 
   onCloseAlert() {
     this.errorMsg = undefined;
     this.moreInfoMsg = undefined;
   }
-
 }
