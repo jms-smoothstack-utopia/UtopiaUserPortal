75436ffe28d3277ee77d29f95d7f9fde2c554047
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-03-05 12:57:35 -0500

    wrote more tests and changed user ID type

--- src/app/user-profile/user-profile.component.spec.ts
+++ src/app/user-profile/user-profile.component.spec.ts
@@ -12,6 +12,12 @@ describe('UserProfileComponent', () => {
   let component: UserProfileComponent;
   let fixture: ComponentFixture<UserProfileComponent>;
   let userServiceSpy: UserService;
+  const expectedFormFields = [
+    'firstNameControl', 'lastNameControl', 'emailControl',
+    'addrLine1Control', 'addrLine2Control', 'cityControl',
+    'stateControl', 'zipCodeControl', 'loyaltyPointsControl',
+    'ticketEmailsControl', 'flightEmailsControl'
+  ];
 
   const mockAddrs: Address[] = [ {
     id: 0,
@@ -22,11 +28,12 @@ describe('UserProfileComponent', () => {
     zipcode: '12345'
   } ];
   const mockUser: User = {
-    id: 1,
+    id: '00000000-0000-0000-0000-000000000000',
     firstName: 'Foo',
     lastName: 'Testuser',
     email: 'foo@example.com',
     addresses: mockAddrs,
+    loyaltyPoints: 1,
     addrLine1: '',
     addrLine2: '',
     city: '',
@@ -50,6 +57,10 @@ describe('UserProfileComponent', () => {
     userServiceSpy = fixture.debugElement.injector.get(UserService);
   });
 
+  afterEach(() => {
+    component.userProfileForm.reset();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
@@ -61,11 +72,43 @@ describe('UserProfileComponent', () => {
   it('checkUser should set the user', () => {
     component.setUser(mockUser);
     expect((component.user as User).firstName).toEqual('Foo');
+    expect((component.user as User).lastName).toEqual('Testuser');
+    expect((component.user as User).email).toEqual('foo@example.com');
+    expect((component.user as User).loyaltyPoints).toEqual(1);
+    expect((component.user as User).ticketEmails).toBeTrue();
+    expect((component.user as User).flightEmails).toBeTrue();
   });
 
-  it('fillThisUserAddr should fill in user address', () => {
+  it('setUser should call fillThisUserAddr, should fill in user address', () => {
     component.setUser(mockUser);
-    //address should have been filled as part of setUser
     expect((component.user as User).addrLine1).toEqual('123 Fake St');
+    expect((component.user as User).addrLine2).toEqual('#1');
+    expect((component.user as User).city).toEqual('Anywhere');
+    expect((component.user as User).state).toEqual('VA');
+    expect((component.user as User).zipcode).toEqual('12345');
+  });
+
+  it('form should create', () => {
+    expect(component.userProfileForm).toBeTruthy();
+  });
+  
+  it('form should have all expected fields', () => {
+    expectedFormFields.forEach((field) => {
+      expect(component.userProfileForm.contains(field)).toBeTrue();
+    });
+  });
+
+  it('form zip code field should be invalid if bad zip code entered', () => {
+    component.userProfileForm.get('zipCodeControl')!.setValue('0');
+    component.userProfileForm.get('zipCodeControl')!.markAsTouched();
+
+    expect(component.userProfileForm.get('zipCodeControl')!.invalid).toBeTrue();
+  });
+
+  it('form email field should be invalid if bad email entered', () => {
+    component.userProfileForm.get('emailControl')!.setValue('Not an email!');
+    component.userProfileForm.get('emailControl')!.markAsTouched();
+
+    expect(component.userProfileForm.get('emailControl')!.invalid).toBeTrue();
   });
 });
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,7 +1,13 @@
 import { Address } from "./address";
 
+/*
+    UUIDs aren't strictly strings. However, since an ID's main purpose in the frontend
+    is to identify the user for things like viewing their profile, and it's not meant
+    to be user-mutable, creating a custom TS type for operating on them seems unnecessary.
+*/
+
 export interface User {
-    id: number,
+    id: string,
     firstName: string,
     lastName: string,
     email: string, 
