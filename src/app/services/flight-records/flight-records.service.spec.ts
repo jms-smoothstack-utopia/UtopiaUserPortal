c07d2a8bc83ae9833e198606edc5b1ecbded57e2
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-03-11 21:12:59 -0500

    outlined ticket service, component tests

--- src/app/services/flight-records/flight-records.service.spec.ts
+++ src/app/services/flight-records/flight-records.service.spec.ts
@@ -1,13 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { FlightRecordsService } from './flight-records.service';
 
 describe('FlightRecordsService', () => {
   let service: FlightRecordsService;
+  let httpTestingController: HttpTestingController;
+  let mockId = '00000000-0000-0000-0000-000000000000';
+
+  //test whether it sends requests
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
     service = TestBed.inject(FlightRecordsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
--- src/app/user-flight-history/user-flight-history.component.spec.ts
+++ src/app/user-flight-history/user-flight-history.component.spec.ts
@@ -1,10 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserFlightHistoryComponent } from './user-flight-history.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FlightRecordsService } from '../services/flight-records/flight-records.service';
+import { Ticket } from '../ticket';
 
 describe('UserFlightHistoryComponent', () => {
   let component: UserFlightHistoryComponent;
   let fixture: ComponentFixture<UserFlightHistoryComponent>;
+  let flightRecordsServiceSpy: FlightRecordsService;
+
+  //mock ticket: most fields filled in, but need to test the pretty print
+  //multiple mock tickets to test if it fills the tickets array
+  //no form to create, but maybe see if the table generation can be tested
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
