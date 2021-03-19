0e777213d16b3c8ef4c3715f35c0663f99fd3738
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-03-19 14:59:51 -0400

    Set production host values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,14 @@
 import { NgxLoggerLevel } from "ngx-logger";
 
+const hostUrl = 'https://services.utopia-air.click';
+
 export const environment = {
   production: true,
-  logLevel: NgxLoggerLevel.DEBUG
+  logLevel: NgxLoggerLevel.DEBUG,
+  authEndpoint: `${hostUrl}/login`,
+  accountsEndpoint: `${hostUrl}/accounts`,
+  airportsEndpoint: `${hostUrl}/airports`,
+  flightsEndpoint: `${hostUrl}/flights`,
+  servicingAreaEndpoint : `${hostUrl}/servicing-area`,
+  hostUrl: hostUrl,
 };
