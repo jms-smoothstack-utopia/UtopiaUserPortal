44d098e7ba7cfa484251af887821f2f439d25525
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-03-20 20:00:52 -0400

    Fix build error, missing environment property

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,10 @@ const hostUrl = 'https://services.utopia-air.click';
 export const environment = {
   production: true,
   logLevel: NgxLoggerLevel.ERROR,
+  mapbox: {
+    accessToken:
+      'pk.eyJ1Ijoiam9yZGFuZGl2aW5hIiwiYSI6ImNrbWYyYzM4bDA4Mzgyb3FocW9xeWp0bzIifQ.VV2Rsag8L9Yohjc5eqlQkg',
+  },
   authEndpoint: `${hostUrl}/login`,
   accountsEndpoint: `${hostUrl}/accounts`,
   airportsEndpoint: `${hostUrl}/airports`,
