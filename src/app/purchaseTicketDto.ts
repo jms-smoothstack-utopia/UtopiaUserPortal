37b953d75fd06effeed19c648ea25bc6bd91244d
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-03-22 11:33:36 -0400

    purchaseTicketDto model

---
+++ src/app/purchaseTicketDto.ts
@@ -0,0 +1,11 @@
+export interface purchaseTicketDto {
+    purchaserId: string,    //UUID
+    flightId: number,
+    tickets: TicketItem[]
+}
+
+export interface TicketItem {
+    seatClass: string,
+    seatNumber: string,
+    passengerName: string
+}
