702e5f233c42d79a082869b0bfe3e71b5ac9eb6b
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-03-22 15:34:22 -0400

    hotfixes for new component/service tests

--- src/app/services/purchase-ticket/purchase-ticket.service.spec.ts
+++ src/app/services/purchase-ticket/purchase-ticket.service.spec.ts
@@ -1,12 +1,36 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import {
+  LoggerConfig,
+  NGXLogger,
+  NGXLoggerHttpService,
+  NgxLoggerLevel,
+  NGXMapperService,
+} from 'ngx-logger';
+import {
+  NGXLoggerHttpServiceMock,
+  NGXMapperServiceMock,
+} from 'ngx-logger/testing';
+import { DatePipe } from '@angular/common';
 import { PurchaseTicketService } from './purchase-ticket.service';
 
 describe('PurchaseTicketService', () => {
   let service: PurchaseTicketService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        NGXLogger,
+        { provide: NGXLoggerHttpService, useClass: NGXLoggerHttpServiceMock },
+        { provide: NGXMapperService, useClass: NGXMapperServiceMock },
+        { provide: LoggerConfig, useValue: { level: NgxLoggerLevel.ERROR } },
+        DatePipe,
+      ],
+    });
     service = TestBed.inject(PurchaseTicketService);
   });
 
--- src/app/shoppingcart/shoppingcart.component.spec.ts
+++ src/app/shoppingcart/shoppingcart.component.spec.ts
@@ -1,6 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShoppingcartComponent } from './shoppingcart.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  LoggerConfig,
+  NGXLogger,
+  NGXLoggerHttpService,
+  NgxLoggerLevel,
+  NGXMapperService,
+} from 'ngx-logger';
+import {
+  NGXLoggerHttpServiceMock,
+  NGXMapperServiceMock,
+} from 'ngx-logger/testing';
+import { DatePipe } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
 
 describe('ShoppingcartComponent', () => {
   let component: ShoppingcartComponent;
@@ -9,6 +24,14 @@ describe('ShoppingcartComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ShoppingcartComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule, AppRoutingModule],
+      providers: [
+        NGXLogger,
+        { provide: NGXLoggerHttpService, useClass: NGXLoggerHttpServiceMock },
+        { provide: NGXMapperService, useClass: NGXMapperServiceMock },
+        { provide: LoggerConfig, useValue: { level: NgxLoggerLevel.ERROR } },
+        DatePipe,
+      ],
     }).compileComponents();
   });
 
