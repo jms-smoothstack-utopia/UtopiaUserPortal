fd67133296f1f8eb8d89c1c58a0204597971a088
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-03-21 21:42:21 -0400

    added cancel ticket component tests

--- src/app/cancel-ticket/cancel-ticket.component.spec.ts
+++ src/app/cancel-ticket/cancel-ticket.component.spec.ts
@@ -16,10 +16,14 @@ import {
 } from 'ngx-logger/testing';
 import { DatePipe } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
+import { FlightRecordsService } from '../services/flight-records/flight-records.service';
+import { of } from 'rxjs';
 
 describe('CancelTicketComponent', () => {
   let component: CancelTicketComponent;
   let fixture: ComponentFixture<CancelTicketComponent>;
+  let flightRecordsServiceSpy: FlightRecordsService;
+  const mockTicketId = 1;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -40,9 +44,29 @@ describe('CancelTicketComponent', () => {
     fixture = TestBed.createComponent(CancelTicketComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    flightRecordsServiceSpy = fixture.debugElement.injector.get(FlightRecordsService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('confirmCancel should call cancelTicketById', () => {
+    const cancelFunction = spyOn(
+      flightRecordsServiceSpy,
+      'cancelTicketById'
+    ).and.returnValue(of({}));
+    
+    component.ticketId = 1;
+    component.confirmCancel();
+
+    expect(cancelFunction).toHaveBeenCalledWith(mockTicketId);
+  });
+
+  it('goBack should emit close event', () => {
+    const emitter = spyOn(component.closeEvent, 'emit');
+    component.goBack();
+    expect(emitter).toHaveBeenCalled();
   });
 });
