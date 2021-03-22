d89e1df58914a74adfce4f2564e186315a4c5a2c
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-03-21 21:24:45 -0400

    added new flight record service tests

--- src/app/services/flight-records/flight-records.service.spec.ts
+++ src/app/services/flight-records/flight-records.service.spec.ts
@@ -17,6 +17,7 @@ import {
 import { DatePipe } from '@angular/common';
 import { FlightRecordsService } from './flight-records.service';
 import { Ticket } from '../../ticket';
+import { Flight } from '../../flight'
 
 describe('FlightRecordsService', () => {
   let service: FlightRecordsService;
@@ -50,6 +51,53 @@ describe('FlightRecordsService', () => {
     timePrettyPrint: '',
   };
   let mockTickets: Ticket[] = [mockTicket1, mockTicket2];
+
+  let mockFlight: Flight = {
+    id: 1,
+    possibleLoyaltyPoints: 50,
+    origin: { 
+              iataId: 'ABC', 
+              name: 'Abc Airport',
+              streetAddress: '123 Fake St',
+              city: 'Anywhere',
+              state: 'TN',
+              zipcode: '12345',
+              servicingArea: { id: 1, areaName: 'Somewhere'}
+            },
+    destination:  { 
+                    iataId: 'XYZ', 
+                    name: 'Xyz Airport',
+                    streetAddress: '789 Fake St',
+                    city: 'Anywhere',
+                    state: 'TN',
+                    zipcode: '12345',
+                    servicingArea: { id: 1, areaName: 'Somewhere'}
+                  },
+    airplane: {
+                id: 1,
+                name: 'Planey McPlaneface',
+                seatConfigurations: [{
+                                      id: 1,
+                                      seatClass: 'First',
+                                      numRows: 1,
+                                      numSeatsPerRow: 2
+                                    }]
+              },
+    seats:  [{
+              id: '1-1A',
+              seatRow: 1,
+              seatColumn: 'A',
+              seatClass: 'First',
+              seatStatus: 'SOLD',
+              price: 1
+            }],
+    creationDateTime: new Date(Date.now()),
+    approximateDateTimeStart: new Date(Date.now() + 1),
+    approximateDateTimeEnd: new Date(Date.now() + 2),
+    flightActive: true,
+    departurePrettyPrint: '', //expect these to be filled in
+    arrivalPrettyPrint: ''
+  }
 
   //test whether it sends requests
 
@@ -97,5 +145,38 @@ describe('FlightRecordsService', () => {
       'http://localhost:8080/tickets/upcoming/' + mockId
     );
     req.flush(mockTickets);
+  });
+
+  it('getTicketById should return ticket', () => {
+    service.getTicketById(1).subscribe((res) => {
+      expect(res.passengerName).toEqual('Foo');
+    });
+
+    const req = httpTestingController.expectOne(
+      'http://localhost:8080/tickets/' + '1'
+    );
+    req.flush(mockTicket1);
+  });
+
+  it('cancelTicketById should return no content', () => {
+    service.cancelTicketById(1).subscribe((res) => {
+      expect(res).toBeTruthy();
+    });
+
+    const req = httpTestingController.expectOne(
+      'http://localhost:8080/tickets/cancel/' + '1'
+    );
+    req.flush(true, { status: 204, statusText: 'No Content' });
+  });
+
+  it('getFlightById should return flight', () => {
+    service.getFlightById(1).subscribe((res) => {
+      expect(res.airplane.name).toEqual('Planey McPlaneface');
+    });
+
+    const req = httpTestingController.expectOne(
+      'http://localhost:8080/flights/' + '1'
+    );
+    req.flush(mockFlight);
   });
 });
